57fc81ab07c3f7739073784aef3bef2c1c9dcce5
Cayaf <corentin.fayac89@gmail.com>
2025-07-08 10:38:05 +0200

    update footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,13 +1,13 @@
-<footer class="w-full bg-beige pt-12 md:pt-16 pb-8">
+<footer class="w-full bg-beige pt-12 md:pt-16 pb-8 px-6 md:px-0">
     <div class="container mx-auto flex flex-col items-center justify-center">
         <div class="grid grid-cols-1 md:grid-cols-2">
-            <div class="flex flex-col space-y-6 border-r-2 border-r-black pr-12">
-                <div class="text-3xl font-bold !no-underline">
+            <div class="flex flex-col space-y-6 md:border-r-2 md:border-r-black md:pr-12">
+                <div class="text-3xl font-bold !no-underline text-center md:text-left">
                     {!! $siteName !!}
                 </div>
                 <div>
-                    <a href="tel:0648587569" class="flex items-center mb-4">
-                        <svg class="size-6 mr-4" width="24" height="24" viewBox="0 0 24 24"
+                    <a href="tel:0648587569" class="grid grid-cols-[minmax(0,max-content)_minmax(0,max-content)] items-center text-sm md:text-base mb-4">
+                        <svg class="size-4 md:size-6 mr-4" width="24" height="24" viewBox="0 0 24 24"
                              xmlns="http://www.w3.org/2000/svg">
                             <path d="M22 12C22 10.6868 21.7413 9.38647 21.2388 8.1731C20.7362 6.95996 19.9997 5.85742 19.0711 4.92896C18.1425 4.00024 17.0401 3.26367 15.8268 2.76123C14.6136 2.25854 13.3132 2 12 2V4C13.0506 4 14.0909 4.20703 15.0615 4.60889C16.0321 5.01099 16.914 5.60034 17.6569 6.34326C18.3997 7.08594 18.989 7.96802 19.391 8.93848C19.7931 9.90918 20 10.9495 20 12H22Z"
                                   fill="currentColor"></path>
@@ -18,19 +18,19 @@
                         </svg>
                         06 48 58 75 69
                     </a>
-                    <a href="mailto:commercial@escapadesfantastiques.com" class="flex items-center">
-                        <svg class="size-6 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <a href="mailto:commercial@escapadesfantastiques.com" class="grid grid-cols-[minmax(0,max-content)_minmax(0,max-content)] items-center text-sm md:text-base">
+                        <svg class="size-4 md:size-6 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path fill="currentColor" fill-rule="evenodd"
                                   d="M7.172,11.334 L10.0016129,13.2687863 L12.73,11.387 L18.844647,17.4201015 C18.6835279,17.47198 18.5117028,17.5 18.3333333,17.5 L1.66666667,17.5 C1.44656147,17.5 1.23642159,17.4573334 1.04406658,17.3798199 L7.172,11.334 Z M20,6.376 L20,15.8333333 C20,16.0799316 19.9464441,16.3140212 19.8503291,16.5246054 L13.856,10.611 L20,6.376 Z M0,6.429 L6.042,10.561 L0.105700422,16.4187119 C0.0373700357,16.2365871 1.90096522e-15,16.0393243 1.77635684e-15,15.8333333 L0,6.429 Z M18.3333333,2.5 C19.2538079,2.5 20,3.24619208 20,4.16666667 L20,4.753 L9.99838709,11.6476481 L0,4.81 L1.77635684e-15,4.16666667 C1.66363121e-15,3.24619208 0.746192084,2.5 1.66666667,2.5 L18.3333333,2.5 Z"></path>
                         </svg>
                         commercial@escapadesfantastiques.com
                     </a>
                 </div>
-                <div class="flex flex-col">
-                    <div class="text-lg font-bold mb-2">
+                <div class="flex flex-col mb-4">
+                    <div class="md:text-lg font-medium md:font-bold mb-2">
                         Suivez les Escapades Fantastiques sur :
                     </div>
-                    <div class="flex items-center space-x-4">
+                    <div class="flex items-center justify-center md:justify-start space-x-4">
                         <a href="https://www.youtube.com/channel/UC2GXZso7BWO-q5z9f9NLwsQ/" target="_blank">
                             <img src="{{ Vite::asset('resources/images/icons/icon-youtube.png') }}" alt=""
                                  class="size-8">
@@ -50,20 +50,20 @@
                     </div>
                 </div>
             </div>
-            <div class="pl-12">
+            <div class="md:pl-12">
                 {!! wp_nav_menu([
                     'menu' => 6,
-                    'menu_class' => 'flex flex-col space-y-4 text-lg font-bold [&_a]:!no-underline',
+                    'menu_class' => 'chevron-right flex flex-col space-y-4 text-lg font-bold [&_a]:!no-underline',
                     'echo' => false,
                 ]) !!}
             </div>
         </div>
-        <div class="flex items-center mt-8">
+        <div class="flex items-center justify-center mt-6">
             <a href="/politique-de-confidentialite/">
                 Politique de confidentialité
             </a>
         </div>
-        <div class="flex items-center mt-8">
+        <div class="flex items-center justify-center text-center mt-8">
             © {{ date('Y') }} Hervé Gourdet. Droits de reproduction réservés
         </div>
     </div>
